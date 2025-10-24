c3a62e3cf0f8e8733829b1138932df39bfaf6988
Rudoruka <Rudoruka@gmail.com>
2025-10-24 23:08:44 +0300

    new

--- src/processing.py
+++ src/processing.py
@@ -2,7 +2,9 @@ from datetime import datetime
 from typing import Any, Dict, List
 
 
-def filter_by_state(dictionary_1: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
+def filter_by_state(
+    dictionary_1: List[Dict[str, Any]], state: str = "EXECUTED"
+) -> List[Dict[str, Any]]:
     """Фильтрует список словарей по значению ключа 'state'"""
     return [item for item in dictionary_1 if item.get("state") == state]
 
@@ -13,8 +15,13 @@ def sort_by_date(
     date_format: str = "%Y-%m-%dT%H:%M:%S.%f",
     reverse: bool = True,
 ) -> List[Dict[str, Any]]:
-    """Сортирует список словарей по дате, учитывая формат даты с временем и миллисекундами"""
-    return sorted(dictionary_2, key=lambda d: datetime.strptime(d[date_key], date_format), reverse=reverse)
+    """Сортирует список словарей по дате,
+    учитывая формат даты с временем и миллисекундами"""
+    return sorted(
+        dictionary_2,
+        key=lambda d: datetime.strptime(d[date_key], date_format),
+        reverse=reverse,
+    )
 
 
 # код сортирует список словарей по дате,
@@ -24,10 +31,26 @@ def sort_by_date(
 
 # Исходный список словарей
 dictionary = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    {
+        "id": 41428829,
+        "state": "EXECUTED",
+        "date": "2019-07-03T18:35:29.512364",
+    },
+    {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+    },
+    {
+        "id": 594226727,
+        "state": "CANCELED",
+        "date": "2018-09-12T21:27:25.241689",
+    },
+    {
+        "id": 615064591,
+        "state": "CANCELED",
+        "date": "2018-10-14T08:21:33.419441",
+    },
 ]
 
 # Фильтруем словарь по состоянию 'EXECUTED'
